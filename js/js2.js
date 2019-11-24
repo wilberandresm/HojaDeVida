939ffe70e6cd49638229c0b4205897549b29f16f
wilberandresm <wilberandresm@hotmail.com>
2019-11-24 12:32:38 -0500

    texto animado

---
+++ js/js2.js
@@ -0,0 +1,12 @@
+$(function() {
+                
+    setTimeout(function() {
+        $('.fly-in-text').removeClass('hidden');
+    }, 500);
+    setTimeout(function() {
+        $('.boton').removeClass('hidden');
+    }, 500);
+
+   
+    
+})();
